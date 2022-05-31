346f538ef0a737b753eff0d73ee6ccaed6a3cb7b
yoo-jimin127 <dbwlals9936@naver.com>
2022-05-31 16:18:10 +0900

    add : typescript example sourcecode

---
+++ Week5_Typescript_practice/class_starter.js
@@ -0,0 +1,11 @@
+"use strict";
+class PersonClass {
+    constructor(age) {
+        this.name = 'front';
+        this.age = age;
+    }
+}
+// 객체 생성
+const person_class1 = new PersonClass(39);
+console.log(person_class1);
+console.log(person_class1.age);
